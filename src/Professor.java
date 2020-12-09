caf80540de62e587566c91e7a47ef60d0b025137
Elygledson Js <47836992+Elygledson@users.noreply.github.com>
2020-12-08 21:30:12 -0300

    Delete Professor.java

--- src/Professor.java
+++
@@ -1,7 +0,0 @@
-public class Professor extends Colaboradores{
-    
-    public Professor(String nome,String tipo,String email,String grau,Integer SecretCode)
-    {
-        super(nome, tipo, email, grau, SecretCode);
-    }
-}

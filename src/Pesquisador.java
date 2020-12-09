347af037dffc4ea4070adb9628dc12cf3e5f47d3
Elygledson Js <47836992+Elygledson@users.noreply.github.com>
2020-12-08 21:30:28 -0300

    Delete Pesquisador.java

--- src/Pesquisador.java
+++
@@ -1,7 +0,0 @@
-public class Pesquisador  extends Colaboradores{
-    
-    public Pesquisador(String nome,String tipo,String email ,String grau,Integer SecretCode)
-    {
-        super(nome, tipo, email, grau, SecretCode);
-    }
-}
